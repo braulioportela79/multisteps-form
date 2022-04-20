1c518eba7bed5df3682d97844e3fc40c7437bb31
braulioportela79 <braulioportela@outlook.com>
2022-04-20 16:48:03 -0300

    FormStep 1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -16,7 +16,12 @@ export const FormStep1 = () => {
   });
 
   const handleNextStep = () => {
-    navigate('/step2');
+      if(state.name !== '') {
+        navigate('/step2');      
+      } else {
+          alert('Enter your information.');
+      }
+    
   };
 
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
