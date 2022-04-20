9b3e7200ecb448a2e73eb88cb6e5f28624d1b860
braulioportela79 <braulioportela@outlook.com>
2022-04-20 16:00:01 -0300

    Step1 Page

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -2,9 +2,23 @@ import * as C from './styles';
 import { Theme } from '../../components/Theme';
 
 export const FormStep1 = () => {
+  const handleNextStep = () => {};
+
   return (
     <Theme>
-      <C.Container>Passo 1</C.Container>
+      <C.Container>
+        <p>Step 1/3</p>
+        <h1>Let's begin with your name</h1>
+        <p>Insert Full Name</p>
+
+        <hr />
+        <label htmlFor=''>
+          Full Name
+          <input type='text' autoFocus />
+        </label>
+
+        <button onClick={handleNextStep}>Next</button>
+      </C.Container>
     </Theme>
   );
 };
