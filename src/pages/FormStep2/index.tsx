f7ac3f8a8d795ab731fa1c5eb7591d4246cb9a6a
braulioportela79 <braulioportela@outlook.com>
2022-04-20 16:51:32 -0300

    FormStep2 Page

--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,9 +1,39 @@
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../components/Theme';
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep2 = () => {
-    return (
-        <C.Container>
-            Passo 2
-        </C.Container>
-    );
-}
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2,
+    });
+  });
+
+  const handleNextStep = () => {
+      if(state.name !== '') {
+        navigate('/step2');      
+      } else {
+          alert('Enter your information.');
+      }
+    
+  };
+
+  return (
+    <Theme>
+      <C.Container>
+        <p>Step 2/3</p>
+        <h1>Let's begin with your name</h1>
+        <p>Insert Full Name</p>
+        <hr />
+
+        <button onClick={handleNextStep}>Next</button>
+      </C.Container>
+    </Theme>
+  );
+};
