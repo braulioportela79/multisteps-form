ac2d22fcaefb61041f158d5871b238b604e924dd
braulioportela79 <braulioportela@outlook.com>
2022-04-20 16:32:01 -0300

    Step1 NameChange Event

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,11 +1,20 @@
 import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
 import { Theme } from '../../components/Theme';
+import { ChangeEvent } from 'react';
 
 export const FormStep1 = () => {
   const navigate = useNavigate();
+  const { state, dispatch } = useForm();
   const handleNextStep = () => {
     navigate('/step2');
+  };
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value,
+    });
   };
 
   return (
@@ -14,13 +23,16 @@ export const FormStep1 = () => {
         <p>Step 1/3</p>
         <h1>Let's begin with your name</h1>
         <p>Insert Full Name</p>
-
         <hr />
         <label>
           Full Name
-          <input type='text' autoFocus />
+          <input
+            type='text'
+            autoFocus
+            value={state.name}
+            onChange={handleNameChange}
+          />
         </label>
-
         <button onClick={handleNextStep}>Next</button>
       </C.Container>
     </Theme>
